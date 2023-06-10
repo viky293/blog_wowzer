1b66f81f3026416763db1ea9514756cc1817d387
Vikram <viky293@gmail.com>
2023-06-11 00:46:08 +0530

    WIP: customizing index page

---
+++ src/templates/index-page-custom.js
@@ -0,0 +1,100 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql } from "gatsby";
+import { getImage } from "gatsby-plugin-image";
+
+import Layout from "../components/Layout";
+import Features from "../components/Features";
+import BlogRoll from "../components/BlogRoll";
+import FullWidthImage from "../components/FullWidthImage";
+
+// eslint-disable-next-line
+export const IndexPageCustomTemplate = ({
+  image,
+  title,
+  heading,
+  subheading,
+ 
+}) => {
+  const heroImage = getImage(image) || image;
+
+  return (
+    <div>
+      <FullWidthImage img={heroImage} title={title} subheading={subheading} />
+      <section className="section section--gradient">
+        <div className="container">
+          <div className="section">
+            <div className="columns">
+              <div className="column is-10 is-offset-1">
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+IndexPageCustomTemplate.propTypes = {
+  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  title: PropTypes.string,
+  heading: PropTypes.string,
+  subheading: PropTypes.string,
+  
+};
+
+const IndexPage = ({ data }) => {
+  const { frontmatter } = data.markdownRemark;
+
+  return (
+    <Layout>
+      <IndexPageCustomTemplate
+        image={frontmatter.image}
+        title={frontmatter.title}
+        heading={frontmatter.heading}
+        subheading={frontmatter.subheading}
+      />
+    </Layout>
+  );
+};
+
+IndexPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
+    }),
+  }),
+};
+
+export default IndexPage;
+
+export const pageQuery = graphql`
+  query IndexPageCustomTemplate {
+    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+      frontmatter {
+        title
+        image {
+          childImageSharp {
+            gatsbyImageData(quality: 100, layout: FULL_WIDTH)
+          }
+        }
+        heading
+        subheading
+        description
+        intro {
+          blurbs {
+            image {
+              childImageSharp {
+                gatsbyImageData(width: 240, quality: 64, layout: CONSTRAINED)
+              }
+            }
+            text
+          }
+          heading
+          description
+        }
+      }
+    }
+  }
+`;
